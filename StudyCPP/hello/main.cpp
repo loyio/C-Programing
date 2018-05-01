7d7ea48ca1d8a8052af184948090e254883b8687
susmote <admin@susmote.com>
2018-05-01 11:34:11 +0800

    hello xcode

---
+++ StudyCPP/hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by susmote on 2018/5/1.
+//  Copyright © 2018年 susmote. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
